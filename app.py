2aa18aed80b3256c9bd8f727fe71608586d5570e
sullvs <dlayelhd@gmail.com>
2026-09-14 00:54:50 +0300

    trainer features completed

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, request, redirect, session, abort
+from werkzeug.security import generate_password_hash, check_password_hash
 from functools import wraps
 from datetime import date
 import sqlite3
-
+import os
 def init_db():
     conn = sqlite3.connect('trainer_app.db') 
     with open('schema.sql') as f:
@@ -11,7 +12,7 @@ def init_db():
     conn.close()
 
 app = Flask(__name__)
-app.secret_key = 'dev-key-change-later'
+app.secret_key = os.environ['SECRET_KEY']
 
 @app.route('/')
 def home():
@@ -48,7 +49,7 @@ def login():
     conn=sqlite3.connect('trainer_app.db')
     check=conn.execute("SELECT password, user_id, role from users WHERE email = ?", (eemail,)).fetchone()
     conn.close()
-    if check is None or ppassword!=check[0]:
+    if check is None or not check_password_hash(check[0],ppassword):
         return render_template("login.html", error="Wrong Email or Password")
             
     session['user_id']=check[1]
@@ -70,8 +71,9 @@ def register():
         birthdate=request.form['birthdate']
         target_weight=request.form['target_weight']
 
+        password_typed = generate_password_hash(password, method='pbkdf2:sha256')
         conn=sqlite3.connect('trainer_app.db')
-        conn.execute("INSERT INTO users (first_name, last_name, password, email, height, birthdate, target_weight, role, registration_date) VALUES (?,?,?,?,?,?,?,?,?)" ,(firstname, lastname, password, email, height, birthdate, target_weight, 'Client', date.today()))
+        conn.execute("INSERT INTO users (first_name, last_name, password, email, height, birthdate, target_weight, role, registration_date) VALUES (?,?,?,?,?,?,?,?,?)" ,(firstname, lastname, password_typed, email, height, birthdate, target_weight, 'Client', date.today()))
         conn.commit()
         conn.close()
         return redirect('/login')
@@ -97,7 +99,26 @@ def trainer_view_client(client_id):
     per_rows_food=conn.execute("SELECT meal_type, meal_content, log_time FROM food_log WHERE user_id = ?", (client_id,)).fetchall()
     per_rows_weight=conn.execute("SELECT current_weight, log_time FROM weight_log WHERE user_id= ?", (client_id,)).fetchall()
     conn.close()
-    return render_template("one_client.html",one_cli_users=per_rows_users, one_cli_food=per_rows_food, one_cli_weight=per_rows_weight)
+    return render_template("one_client.html",one_cli_users=per_rows_users, one_cli_food=per_rows_food, one_cli_weight=per_rows_weight, client_id=client_id)
+
+@app.route('/trainer/client/<client_id>/assign', methods=['POST','GET'])
+@login_required
+@role_required('Trainer')
+def trainer_assign_splits(client_id):
+    if request.method=='POST':
+        num_of_dayss=request.form['num_of_days']
+        suitable_workout_splitt=request.form['split']
+        conn=sqlite3.connect("trainer_app.db")
+        conn.execute("INSERT INTO workout (num_of_days, suitable_workout_split, log_time, user_id) VALUES (?, ?, ?, ?)", (num_of_dayss, suitable_workout_splitt, date.today(),client_id))
+        conn.commit()
+        conn.close()
+        return redirect(f'/trainer/client/{client_id}')
+    elif request.method=='GET':
+        conn=sqlite3.connect("trainer_app.db")
+        workout_val=conn.execute("SELECT num_of_days, suitable_workout_split, log_time FROM workout WHERE user_id = ?", (client_id,)).fetchall()
+        conn.close()
+        return render_template("assign.html", val=workout_val)
+
 
 @app.route('/client/dashboard', methods=['POST','GET'])
 @login_required
